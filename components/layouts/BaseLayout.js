4d3013ec841c5e721df9c6844bdfb5e66cee2529
gitfilix <runlevel07@gmail.com>
2019-05-01 15:16:56 +0200

    shared components also

---
+++ components/layouts/BaseLayout.js
@@ -0,0 +1,16 @@
+// layout component
+import React from 'react';
+import Header from '../shared/Header';
+
+
+const BaseLayout = (props) => {
+    return (
+        <React.Fragment>
+        <Header />
+            { props.children }
+        </React.Fragment>
+    );
+}
+
+
+export default BaseLayout;
